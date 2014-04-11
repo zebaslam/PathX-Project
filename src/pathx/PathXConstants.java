bb816a9b11b6e3d5230ce521a38042b305dd92e2
paine724 <oblivionlionheart724@gmail.com>
2014-04-10 23:51:26 -0400

    Created constants and extended minigame from minigame class

--- src/pathx/PathXConstants.java
+++ src/pathx/PathXConstants.java
@@ -5,11 +5,49 @@
  */
 
 package pathx;
+import java.awt.Color;
+import java.awt.Font;
 
 /**
- *
- * @author zeb
+ *This class stores all the constants used by The PathX application. We'll
+ * do this here rather than load them from files because many of these are
+ * derived from each other.
+ * @author Zeb Aslam (108041523)
+ * @author Richard Mckenna 
  */
 public class PathXConstants {
+    // WE NEED THESE CONSTANTS JUST TO GET STARTED
+    // LOADING SETTINGS FROM OUR XML FILES
+    public static String PROPERTY_TYPES_LIST = "property_types.txt";
+    public static String PROPERTIES_FILE_NAME = "properties.xml";
+    public static String PROPERTIES_SCHEMA_FILE_NAME = "properties_schema.xsd";    
+    public static String PATH_DATA = "./data/";
     
+    // THESE ARE THE TYPES OF CONTROLS, WE USE THESE CONSTANTS BECAUSE WE'LL
+    // STORE THEM BY TYPE, SO THESE WILL PROVIDE A MEANS OF IDENTIFYING THEM
+    
+    // EACH SCREEN HAS ITS OWN BACKGROUND TYPE
+    public static final String BACKGROUND_TYPE = "BACKGROUND_TYPE";
+    
+    // THIS REPRESENTS THE BUTTONS ON THE MENU SCREEN FOR LEVEL SELECTION
+    public static final String LEVEL_SELECT_BUTTON_TYPE = "LEVEL_SELECT_BUTTON_TYPE";
+    
+    // WE'LL USE THESE STATES TO CONTROL SWITCHING BETWEEN THE TWO
+    public static final String MENU_SCREEN_STATE = "MENU_SCREEN_STATE";
+    public static final String GAME_SCREEN_STATE = "GAME_SCREEN_STATE";   
+    
+    public static final int FPS = 30;
+
+    // UI CONTROL SIZE AND POSITION SETTINGS
+    public static final int WINDOW_WIDTH = 640;
+    public static final int WINDOW_HEIGHT = 480;
+    public static final int VIEWPORT_MARGIN_LEFT = 20;
+    public static final int VIEWPORT_MARGIN_RIGHT = 20;
+    public static final int VIEWPORT_MARGIN_TOP = 20;
+    public static final int VIEWPORT_MARGIN_BOTTOM = 20;
+    public static final int LEVEL_BUTTON_WIDTH = 200;
+    public static final int LEVEL_BUTTON_MARGIN = 5;
+    public static final int LEVEL_BUTTON_Y = 570;
+    public static final int VIEWPORT_INC = 5;
+        
 }
--- src/pathx/ui/PathXMiniGame.java
+++ src/pathx/ui/PathXMiniGame.java
@@ -5,11 +5,70 @@
  */
 
 package pathx.ui;
-
+import java.awt.Cursor;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JFrame;
+import pathx.data.PathXDataModel;
+import mini_game.MiniGame;
+import mini_game.MiniGameState;
+import static pathx.PathXConstants.*;
+import mini_game.Sprite;
+import mini_game.SpriteType;
+import mini_game.Viewport;
+import properties_manager.PropertiesManager;
+import pathx.PathXConstants;
+import pathx.PathX.PathXPropertyType;
+//import sorting_hat.file.SortingHatFileManager;
+//import sorting_hat.data.SortingHatRecord;
 /**
  *
  * @author zeb
  */
-public class PathXMiniGame {
+public class PathXMiniGame extends MiniGame{
+
+    @Override
+    public void initAudioContent() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void initData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void initGUIControls() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void initGUIHandlers() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void reset() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateGUI() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
